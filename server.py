7e1b31fd8719f31b37f61b0fedab6eacaf305a37
Nathan Touchton <ntouchton@protonmail.com>
2023-06-16 13:52:07 -0500

    modified:   server.py 	modified:   templates/index.html 	new file:   templates/send.html

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template, request
 from emailtemplate import SendEmail
 
 app = Flask(__name__)
-
 send = SendEmail(smtp_server="smtp.mail.yahoo.com")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/send", methods=["POST"])
 def send_email():
     send.send_message(
         to_email=environ["TO_EMAIL"],
-        message=f"Inquiry from {request.form['name']}:\n\n{request.form['message']}"
+        message=f"Subject:Inquiry from {request.form['name']}\n\n{request.form['message']}"
     )
+    return render_template("send.html")
